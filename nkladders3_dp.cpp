e27c796212c42bcc101d8dd698af819d11bd6daf
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2021-12-29 11:38:55 +1100

    nkladder practice

--- nkladders3_dp.cpp
+++ nkladders3_dp.cpp
@@ -2,22 +2,23 @@
 
 using namespace std;
 
-int countWaysTD(int n, int k, int *dp) {
+int countWays(int n, int k, int *dp) {
     if (n == 0) return 1;
     if (n < 0) return 0;
     if (dp[n] != 0) return dp[n];
-    
+
     int ans = 0;
     for (int jump = 1; jump <= k; jump++) {
-        ans += countWaysTD(n - jump, k, dp);
+        ans += countWays(n - jump, k, dp);
     }
     return dp[n] = ans;
 }
 
 int main(void) {
     int n, k;
-    int dp[1000] = {0};
     cin >> n >> k;
-    cout << countWaysTD(n, k, dp) << endl;
+    int dp[1000] = {0};
+    cout << countWays(n, k, dp) << endl;
+
     return 0;
 }
--- nkladders4.cpp
+++ nkladders4.cpp
@@ -2,22 +2,22 @@
 
 using namespace std;
 
-int countWaysTD(int n, int k, int *dp) {
+int countWays(int n, int k) {
     if (n == 0) return 1;
     if (n < 0) return 0;
-    if (dp[n] != 0) return dp[n];
-    
+
     int ans = 0;
     for (int jump = 1; jump <= k; jump++) {
-        ans += countWaysTD(n - jump, k, dp);
+        ans += countWays(n - jump, k);
     }
-    return dp[n] = ans;
+
+    return ans;
 }
 
 int main(void) {
     int n, k;
-    int dp[1000] = {0};
     cin >> n >> k;
-    cout << countWaysTD(n, k, dp) << endl;
+    cout << countWays(n, k) << endl;
+
     return 0;
 }
