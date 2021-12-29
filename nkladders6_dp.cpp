2fb90383ffe638a116aba5ac59a6ee8f6d9cb6d7
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2021-12-29 14:35:53 +1100

    nkladders practice

---
+++ nkladders6_dp.cpp
@@ -0,0 +1,25 @@
+//using dynamic programming
+#include <iostream>
+using namespace std;
+
+int countWays(int n, int k, int *dp) {
+    if (n == 0) return 1;
+    if (n < 0) return 0;
+    if (dp[n] != 0) return dp[n];
+
+    int ans = 0;
+    for (int jump = 1; jump <= k; jump++) {
+        ans += countWays(n-jump, k, dp);
+    }
+
+    return dp[n] = ans;
+}
+
+int main(void) {
+    int n, k;
+    cin >> n >> k;
+    int dp[100] = {0};
+    cout << countWays(n, k, dp) << endl;
+
+    return 0;
+}
---
+++ nkladdersBU3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+int countWaysBU(int n, int k) {
+    vector<int> dp(n+1, 0);
+
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int jump = 1; jump <= k; jump++) {
+            if (i - jump >= 0) {
+                dp[i] += dp[i-jump];
+            }
+        }
+    }
+
+    return dp[n];
+}
+
+int main(void) {
+    int n, k;
+    cin >> n >> k;
+    cout << countWaysBU(n, k) << endl;
+
+    return 0;
+}
