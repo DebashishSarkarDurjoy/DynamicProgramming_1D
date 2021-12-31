a82f8b206bad49b8f7d990bac80946e4c3220475
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2021-12-31 12:28:51 +1100

    nkladders practice

---
+++ nkladdersBU4.cpp
@@ -0,0 +1,28 @@
+// bottom up approach
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int countWays(int n, int k) {
+    vector<int> dp(n+1, 0);
+
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int jump = 1; jump <= k; jump++) {
+            if (i - jump >= 0) dp[i] += dp[i-jump];
+        }
+    }
+
+    return dp[n];
+}
+
+int main(void) {
+    int n, k;
+    cin >> n >> k;
+
+    cout << countWays(n, k) << endl;
+
+    return 0;
+}
