9badffb1bd7d607ca2ec8901cfd253ba93b0c620
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2022-01-30 12:17:45 +1100

    coinChange practice 2

---
+++ coinChange2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int minCoinChange(int m, vector<int> denoms) {
+    vector<int> dp(m+1, 0);
+    dp[0] = 0;
+
+    for (int i = 1; i <= m; i++) {
+        dp[i] = INT_MAX;
+        for (int c : denoms) {
+            if (i - c >= 0 && dp[i - c] != INT_MAX) {
+                dp[i] = min(dp[i], dp[i-c] + 1);
+            }
+        }
+    }
+    return dp[m] == INT_MAX ? -1 : dp[m];
+}
+
+int main(void) {
+    vector<int> denoms = {1,5,7,10};
+    cout << minCoinChange(16, denoms) << endl;
+
+    return 0;
+}
