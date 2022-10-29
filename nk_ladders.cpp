9d27cb8ab9f23ee00c9a681ef7328d5c8f91e0e6
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2022-10-29 22:00:17 +1100

    nk-ladders practice top down memoisation

--- nk_ladders.cpp
+++ nk_ladders.cpp
@@ -16,12 +16,28 @@ int countWays(int n, int k) {
     return ways;
 }
 
+int countWaysTD(int n, int k, int lookup[]) {
+    // base case
+    if (n == 0) return 1;
+    if (lookup[n] != 0) return lookup[n];
 
+    // recursive case
+    int ways = 0;
+    for (int i = 1; i <= k; i++) {
+        if (n-i >= 0) {
+            ways += countWaysTD(n-i, k, lookup);
+        }
+    }
+    lookup[n] = ways;
+    return ways;
+}
 
 int main(void) {
     int n, k;
     cin >> n >> k;
-    cout << countWays(n, k)<< endl;
+    // cout << countWays(n, k)<< endl;
 
+    int lookup[100] = { 0 };
+    cout << countWaysTD(n, k, lookup) << endl;
     return 0;
 }
